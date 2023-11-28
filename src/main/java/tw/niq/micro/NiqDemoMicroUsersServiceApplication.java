d103cbb175f68341710289642acec918f3f9dcde
Nick Chen <nick.chen@cmp.com.tw>
2023-11-28 10:24:11 +0800

    Add eureka client

--- src/main/java/tw/niq/micro/NiqDemoMicroUsersServiceApplication.java
+++ src/main/java/tw/niq/micro/NiqDemoMicroUsersServiceApplication.java
@@ -2,7 +2,9 @@ package tw.niq.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NiqDemoMicroUsersServiceApplication {
 
